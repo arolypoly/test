4e8264168e479847c236f112c1e5abf23e725e55
W <dangwilliam3@gmail.com>
2015-08-11 21:21:10 -0700

    added new page?

---
+++ client/app/scripts/controllers/potato.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:PotatoCtrl
+ * @description
+ * # PotatoCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('PotatoCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
